6ed555c0b9d2be63cf0670b04d1743507b4a0413
Gabriel Chica Fernandez <gchicafernandez@guidewire.com>
2019-12-12 22:21:25 +0100

    Add support for exercises that only have repetitions (e.g. Pull-ups)

--- src/training_session.py
+++ src/training_session.py
@@ -22,19 +22,30 @@ class TrainingSession(object):
         return weight
     
     def _get_breakdown(self, comment_text):
-        parts = comment_text.split("\n")
+        lines = comment_text.split("\n")
         series = []
-        lastWeight = None
-        for part in parts:
-            all_numbers = RE_MATCHER.findall(part)
+        numbersInComment = []
+        for line in lines:
+            all_numbers = RE_MATCHER.findall(line)
             if len(all_numbers) == 0:
                 continue
 
             number = float(all_numbers[0])
-            if number > MAX_REPETITIONS:
-                lastWeight = self._ensure_kg(number)
-            else:
-                series.append([lastWeight, number])
+            numbersInComment.append(number)
+
+        commentOnlyHasRepetitions = not (any(number > MAX_REPETITIONS for number in numbersInComment) \
+            and any(number <= MAX_REPETITIONS for number in numbersInComment))
+        
+        if (commentOnlyHasRepetitions):
+            for number in numbersInComment:
+                series.append([0.0, number])
+        else:
+            lastWeight = None
+            for number in numbersInComment:
+                if number > MAX_REPETITIONS:
+                    lastWeight = self._ensure_kg(number)
+                else:
+                    series.append([lastWeight, number])
         
         return series
     
